9a225b27a1edf578b9db02ce1bca5949ea58c9ec
maxmwamba68 <mnm9803@nyu.edu>
2026-02-03 15:55:08 -0600

    Update gettingStarted.py
    Refactored welcome_assignment_answers to use if-elif structure instead of a dictionary for answers.

--- gettingStarted.py
+++ gettingStarted.py
@@ -1,22 +1,44 @@
+### welcome_assignment_answers
+### Input - All nine questions given in the assignment.
+### Output - The right answer for the specific question.
+
 def welcome_assignment_answers(question):
-    # Using a dictionary for cleaner "case-like" implementation
-    answers = {
-        "Are encoding and encryption the same? - Yes/No": "No",
-        "Is it possible to decrypt a message without a key? - Yes/No": "No",
-        "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?": "pcap",
-        "Is it possible to decode a message without a key? - Yes/No": "Yes",
-        "Is a hashed message supposed to be un-hashed? - Yes/No": "No",
-        "What is the SHA256 hashing value of your NYU email and use the answer in your code - ": "c6657f002b793eff45a324aad909a8f4963250df50144013f99a8aba188ae1ba",
-        "Is MD5 a secured hashing algorithm? - Yes/No": "No",
-        "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number": 5,
-        "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number": 3
-    }
-    
-    # .get() allows us to provide the default "else" message if the question isn't found
-    return answers.get(question, "This is not my beautiful wife! This is not my beautiful car! How did I get here?")
+    #Students do not have to follow the skeleton for this assignment.
+    #Another way to implement is using a "case" statements similar to C.
+    if question == "Are encoding and encryption the same? - Yes/No":
+        answer = "No"
+    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+        answer = "No"
+    elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+        answer = "pcap"
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "Yes"
+    elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ": 
+        answer = "c6657f002b793eff45a324aad909a8f4963250df50144013f99a8aba188ae1ba"
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer = "No"
+    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+        answer = 5
+    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+        answer = 3
+    else: 
+        return answers.get(question, "This is not my beautiful wife! This is not my beautiful car! How did I get here?")
+# Complete all the questions.
+
+
 
 if __name__ == "__main__":
-    # Test a sample question
+    #use this space to debug and verify that the program works
     debug_question = "Are encoding and encryption the same? - Yes/No"
-    print(f"Question: {debug_question}")
-    print(f"Answer: {welcome_assignment_answers(debug_question)}")
+    print(welcome_assignment_answers(debug_question))
+                                                                                                                                                                    
+#Questions:
+#"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+#"Are encoding and encryption the same? - Yes/No":
+#"Is it possible to decrypt a message without a key? - Yes/No":
+#"Is it possible to decode a message without a key? - Yes/No":
+#"Is a hashed message supposed to be un-hashed? - Yes/No":
+#"What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+#"Is MD5 a secured hashing algorithm? - Yes/No":
+#"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+#"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
